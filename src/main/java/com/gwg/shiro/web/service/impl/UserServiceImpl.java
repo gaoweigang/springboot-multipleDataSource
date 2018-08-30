616b33ba3ed3dee184cebaaad3d9c31e2f6170d3
gaoweigang <13817191469@163.com>
2018-08-30 14:57:10 +0800

    Springboot使用Cglib生成代理对象

--- src/main/java/com/gwg/shiro/web/service/impl/UserServiceImpl.java
+++ src/main/java/com/gwg/shiro/web/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.gwg.shiro.web.service.impl;
 
 import com.github.pagehelper.PageInfo;
+import com.gwg.shiro.web.dao.RoleDao;
 import com.gwg.shiro.web.dao.UserDao;
 import com.gwg.shiro.web.dto.UserDto;
 import com.gwg.shiro.web.exception.BusinessException;
@@ -26,8 +27,15 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserDao userDao;
 
+
 	@Override
 	public boolean addUser(UserDto dto) {
+		/**
+		 * 获取Spring代理生成的类的全路径名
+		 * 如果采用的是CGLIB的方式实现AOP，由于是直接修改UserDaoImpL的字节码，所以路径名为com.gwg.shiro.web.service.impl.UserServiceImpl
+		 * 如果采用的是SpringAop的方式实现AOP，由于SpringAOP是基于java接口的，所以路径类似于com.sun.proxy.$Proxy78
+		 */
+		logger.info("获取Spring代理生成的类的全路径名**********:{}", userDao.getClass().getName());
 		return userDao.addUser(dto);
 	}
 
